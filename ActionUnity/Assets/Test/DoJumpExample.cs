e820761ce5debe0b8f17c963a075de9f60cb0831
thantieuhodo <toinguyenkhac@gmail.com>
2016-06-17 20:26:20 +0900

    FirstExample: Jump

---
+++ ActionUnity/Assets/Test/DoJumpExample.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class DoJumpExample : MonoBehaviour {
+
+	[SerializeField]
+	Transform target;
+
+	[SerializeField]
+	float power = 1.0f;
+
+	[SerializeField]
+	int numberJump = 1;
+
+	[SerializeField]
+	float duration = 1.0f;
+
+	[SerializeField]
+	bool snapping = false;
+
+
+	[SerializeField]
+	Ease type = Ease.Linear;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+			transform.DOJump(target.position, power, numberJump, duration, snapping).SetEase(type);
+		}
+
+		if (Input.GetKeyDown(KeyCode.R)) {
+			transform.DOKill();
+			transform.position = Vector3.zero;
+		}
+
+	}
+
+
+}
